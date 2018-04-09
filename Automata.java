95ca314bb2ba13c35d2fcb7d5efebbac81a93cdb
xtrs84zk <xtrs84zk@gmail.com>
2018-04-08 21:36:38 -0500

    Versión alfa

--- Automata.java
+++ Automata.java
@@ -7,7 +7,7 @@ public class Automata {
         String tokens[] = new String[100];
         inicializarTokens(tokens);
         ArrayList<String> codigoPorLineas;
-        ArrayList<String> analisisLexicoDeTodasLasLineas = new ArrayList();
+        ArrayList<String> analisisLexicoDeTodasLasLineas;
         try {
             codigoPorLineas = cargarCodigoDesdeUnArchivoDeTexto();
         } catch (IOException e) {
@@ -18,6 +18,7 @@ public class Automata {
         System.out.println("El archivo de texto se ha cargado correctamente.");
 
         //Analizar línea por línea
+        analisisLexicoDeTodasLasLineas = new ArrayList();
         int i = 0;
         try {
             do {
@@ -48,24 +49,23 @@ public class Automata {
 
     /**
      * Se encarga de leer un archivo previamente especifícado en codificación UTF-8
+     *
      * @return ArrayList con el contenido del archivo
      * @throws IOException en caso de no poder acceder o leer el mismo.
      */
     private static ArrayList cargarCodigoDesdeUnArchivoDeTexto() throws IOException {
-        ArrayList<String> codigoPorLineas = new ArrayList<>();
+        ArrayList<String> codigoPorLineas;
         File archivoDelCodigo = new File("C:/users/xtrs84zk/desktop/codigo.txt");
         InputStreamReader input;
         input = new InputStreamReader(new FileInputStream(archivoDelCodigo), "UTF8");
-        if (input != null) {
-            codigoPorLineas = new ArrayList<>();
-            String r;
-            BufferedReader in = new BufferedReader(input);
-            //el código será leído en minúsculas
-            while ((r = in.readLine()) != null) {
-                codigoPorLineas.add(r.toLowerCase());
-            }
-            in.close();
+        codigoPorLineas = new ArrayList<>();
+        String r;
+        BufferedReader in = new BufferedReader(input);
+        //el código será leído en minúsculas
+        while ((r = in.readLine()) != null) {
+            codigoPorLineas.add(r.toLowerCase());
         }
+        in.close();
         return codigoPorLineas;
     }
 
@@ -138,9 +138,10 @@ public class Automata {
      * por espacios en blanco. Si encuentra una comilla, comienza a verificar la cadena String.
      * De otra forma, busca identificadores o constantes. En caso de no encontrar similitud, se
      * define como un error y continua con la siguiente palabra.
+     *
      * @param lineaAIdentificar que contiene una sola línea String
-     * @param tokens referencia donde se almacenan los tokens del lenguaje
-     * @param numeroDeLinea que es el número de la línea actual
+     * @param tokens            referencia donde se almacenan los tokens del lenguaje
+     * @param numeroDeLinea     que es el número de la línea actual
      * @return un ArrayList que contiene los análisis uno a uno de cada elemento de la línea en cuestión
      */
     private static ArrayList identificarLexicoEnUnaLinea(String lineaAIdentificar, String[] tokens, int numeroDeLinea) {
@@ -185,9 +186,10 @@ public class Automata {
     /**
      * Busca el identificador en un conjunto de tokens predefinido, en caso de encontrarlo,
      * se considera un elemento del lenguaje; regresa el análisis léxico que corresponde.
+     *
      * @param posibleIdentificador a buscar su token
-     * @param tokens donde se buscará el identificador
-     * @param numeroDeLinea en que se encuentra
+     * @param tokens               donde se buscará el identificador
+     * @param numeroDeLinea        en que se encuentra
      * @return análisis de la línea o null en caso de no encontrarse entre los elementos
      */
     private static String analisisLexicoDeElementosDelLenguaje(String posibleIdentificador, String[] tokens, int numeroDeLinea) {
@@ -200,10 +202,11 @@ public class Automata {
 
     /**
      * Compone la línea de análisis con base en los parámetros que recibe.
-     * @param elemento el elemento identificado
-     * @param token que le corresponde al elemento según el lenguaje
+     *
+     * @param elemento       el elemento identificado
+     * @param token          que le corresponde al elemento según el lenguaje
      * @param tipoDeElemento ya sea -1 o -2
-     * @param numeroDeLinea en que fue encontrado el elemento
+     * @param numeroDeLinea  en que fue encontrado el elemento
      * @return una cadena preformada legible al usuario
      */
     private static String analisisLexico(String elemento, int token, int tipoDeElemento, int numeroDeLinea) {
@@ -214,12 +217,13 @@ public class Automata {
      * Recibe un arreglo y la cadena que buscará en él. En caso de encontrarlo, regresa
      * su posición en el arreglo; de lo contrario, regresa -100 como mensaje de error.
      *
-     * @param tokens bloque que contiene los tokens del lenguaje
+     * @param tokens               bloque que contiene los tokens del lenguaje
      * @param loQueDeberiaContener cadena que debería encontrase en dicho bloque
      * @return el número del token que corresponde al elemento
      */
     private static int obtenerToken(String[] tokens, String loQueDeberiaContener) {
         loQueDeberiaContener = loQueDeberiaContener.replaceAll("\n", "");
+        loQueDeberiaContener = loQueDeberiaContener.replaceAll(" ", "");
         for (int i = 0; i < 100; i++) {
             if (tokens[i] != null) {
                 if (tokens[i].equals(loQueDeberiaContener)) {
@@ -235,8 +239,9 @@ public class Automata {
      * se procede a analizar una posible constante entera o real. Encontrando letras, un
      * identificador que puede tener, como máximo, seis caracteres.
      * Si lo anterior falla, regresa null.
+     *
      * @param posibleIdentificador a analizar
-     * @param numeroDeLinea en que se encuentra
+     * @param numeroDeLinea        en que se encuentra
      * @return análisis léxico de el elemento
      */
     private static String analisisLexicoDeIdentificadores(String posibleIdentificador, int numeroDeLinea) {
@@ -263,14 +268,15 @@ public class Automata {
      * Analiza una posible cadena String, si comienza con comillas y termina con las mismas, se puede asumir
      * que quizá está correctamente formulada y procede un análisis más minucioso; en caso contrario, se
      * descarta inmediatamente.
+     *
      * @param constante a analizar
      * @return true o false dependiendo de el estado de la cadena
      */
     private static boolean constanteStringCorrectamenteFormulada(String constante) {
-        constante = constante.replaceAll(" ","");
+        constante = constante.replaceAll(" ", "");
         if (constante.charAt(0) == '\"' && constante.charAt(constante.length() - 1) == '\"') {
             int i = 0;
-            do{
+            do {
                 i++;
                 //Si el caracter actual fue una comilla, no es el último caracter en la constante y antes de él
                 //no estaba un caracter de escape, la constante String está mal formada
@@ -291,10 +297,11 @@ public class Automata {
     }
 
     /**
-     *  Analiza la constante recibida para detectar su composición, en caso de tener
-     *  sólo un punto, es correcta-real; en caso de contener más de uno, es un error.
-     *  Si no contiene puntos, es una constante entera.
-     *  Además, analiza que no haya caracteres desconocidos (aparte de números)
+     * Analiza la constante recibida para detectar su composición, en caso de tener
+     * sólo un punto, es correcta-real; en caso de contener más de uno, es un error.
+     * Si no contiene puntos, es una constante entera.
+     * Además, analiza que no haya caracteres desconocidos (aparte de números)
+     *
      * @param constante almacena la cadena a verificar.
      * @return retorna la cantidad de puntos que contiene la constante o 2, en caso de error.
      */
@@ -311,7 +318,7 @@ public class Automata {
                 //Se encontró un punto
                 if (constante.charAt(i) == '.') {
                     //El punto está al final de la cadena, se considera un error.
-                    if(i==constante.length()-1){
+                    if (i == constante.length() - 1) {
                         return 2;
                     }
                     cantidadDePuntosEnLaConstante++;
@@ -329,6 +336,7 @@ public class Automata {
 
     /**
      * Recibe como parámetro un ArrayList y lo escribe a un archivo.
+     *
      * @param analisisLexico que contiene la información a escribir.
      * @throws IOException en caso de no poder escribir al archivo.
      */
