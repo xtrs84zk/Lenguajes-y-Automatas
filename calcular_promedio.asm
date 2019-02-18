704d880b8fd06bb7f9f1fa956b92e961dc05bde8
Xavier Sánchez <el.xyzko@gmail.com>
2019-02-17 18:52:52 -0600

    Add files via upload

--- calcular_promedio.asm
+++ calcular_promedio.asm
@@ -4,17 +4,17 @@
 loop	get
 jz	done
 add	sum
-st sum
+st	sum
 ld	one
-add cant
-j loop
-done ld sum
+add	cant
+j	loop
+done ld	sum
 div cant	prom
 ld	prom
 out
 halt
-zero const 0
-one const 1
-sum const c
-prom const d
-cant const f
+zero	const	0
+one	const	1
+sum	const	c
+prom	const	d
+cant	const	f
