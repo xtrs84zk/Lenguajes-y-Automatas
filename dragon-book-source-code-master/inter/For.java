eab86cb4ad183b02d91a9c3d396e9adf786bd7be
Xavier Sánchez <xtrs84zk@gmail.com>
2019-05-31 07:16:59 -0500

    Well, that's all
    Signed-off-by: xtrs84zk <xtrs84zk@gmail.com>

--- dragon-book-source-code-master/inter/For.java
+++ dragon-book-source-code-master/inter/For.java
@@ -15,7 +15,8 @@ public class For extends Stmt {
     public void init(Expr x, Stmt s) {
         expr = x;
         stmt = s;
-        if (expr.type != Type.Bool) expr.error("boolean required in for");
+        //if (expr.type != Type.Bool) expr.error("boolean required in for");
+
     }
 
     public void gen(int b, int a) {
